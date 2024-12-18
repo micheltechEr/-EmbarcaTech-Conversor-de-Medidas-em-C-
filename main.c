a39c797555f02de74105d10393506b8390b8a4ff
Lucas Gabriel Ferreira <lgferreira.cic@uesc.br>
2024-12-18 18:17:53 -0300

    Alteração da função exibirMenu

--- main.c
+++ main.c
@@ -24,10 +24,15 @@ int main() {
 
 void exibirMenu() {
     printf("\n===== Conversor de Unidades =====\n");
-    printf("1. Converter metros para quilometros\n");
-    printf("2. Converter graus Celsius para Fahrenheit\n");
-    printf("3. Converter quilogramas para libras\n");
-    printf("4. Converter litros para mililitros\n");
+    printf("1. Unidades de comprimento (metro, centímetro, milímetro)\n");
+    printf("2. Unidades de massa (quilograma, grama, tonelada)\n");
+    printf("3. Unidades de volume (litro, mililitro, metros cúbicos)\n");
+    printf("4. Unidades de temperatura (Celsius, Fahrenheit, Kelvin)\n");
+    printf("5. Unidades de velocidade (km/h, m/s, mph)\n");
+    printf("6. Watts (W), quilowatts (kW), cavalos-vapor (cv ou hp)");
+    printf("7. Unidades de área (metro quadrado, centímetro quadrado)\n"); 
+    printf("8. Unidades de tempo (segundos, minutos, horas)\n");
+    printf("9. Bits, bytes, kilobytes (KB), megabytes (MB), gigabytes (GB), terabytes (TB)\n");
     printf("0. Sair\n");
     printf("=================================\n");
 }
