8b874f1805855bbc9c101a4a76db17714b609e1a
Maria Luiza Brasil Medrado <marialuizabrasilmedrado@gmail.com>
2024-12-20 22:17:08 -0300

    Conversor de Unidades de Massa

--- main.c
+++ main.c
@@ -5,15 +5,92 @@ void exibirMenu();
 
 int main() {
     int escolha;
+    float valor;
+    float resultado;
 
     do {
+        
+        printf("Digite o numero desejado\n");
+        scanf("%f", &valor);
+
         exibirMenu();
 
         printf("Escolha uma opcao (0 para sair): ");
         scanf("%d", &escolha);
 
-        if (escolha != 0) {
-            printf('Obrigado por escolher a opcao');
+        switch (escolha){
+            case 1 :{
+
+                printf("\n===== Conversor de quilogramas para gramas =====\n");
+
+                resultado = valor*1000;
+
+                printf("VALOR EM GRAMAS: ");
+                printf("%.4f ", resultado);
+                printf("gramas\n");
+
+                break;
+            }
+
+            case 2 :{
+                printf("\n===== Conversor de gramas para quilogramas =====\n");
+
+                resultado = valor/1000;
+
+                printf("VALOR EM QUILOGRAMAS: ");
+                printf("%.4f ", resultado);
+                printf("quilogramas\n");
+
+                break;
+            }
+
+            case 3 :{
+                printf("\n===== Conversor de quilogramas para toneladas =====\n");
+
+                resultado = valor/1000;
+
+                printf("VALOR EM TONELADAS: ");
+                printf("%.4f ", resultado);
+                printf("toneladas\n");
+
+                break;
+            }
+
+            case 4 :{
+                printf("\n===== Conversor de gramas para toneladas =====\n");
+
+                resultado = valor/1000000;
+
+                printf("VALOR EM TONELADAS: ");
+                printf("%.8f ", resultado);
+                printf("toneladas\n");
+
+                break;
+            }
+
+            case 5 :{
+                printf("\n===== Conversor de toneladas para quilogramas =====\n");
+
+                resultado = valor*1000;
+
+                printf("VALOR EM TONELADAS: ");
+                printf("%.4f ", resultado);
+                printf("quilogramas\n");
+
+                break;
+            }
+
+            case 6 :{
+                printf("\n===== Conversor de toneladas para gramas =====\n");
+
+                resultado = valor*1000000;
+
+                printf("VALOR EM TONELADAS: ");
+                printf("%.4f ", resultado);
+                printf("gramas\n");
+
+                break;
+            }
         }
 
     } while (escolha != 0);
@@ -24,10 +101,12 @@ int main() {
 
 void exibirMenu() {
     printf("\n===== Conversor de Unidades =====\n");
-    printf("1. Converter metros para quilometros\n");
-    printf("2. Converter graus Celsius para Fahrenheit\n");
-    printf("3. Converter quilogramas para libras\n");
-    printf("4. Converter litros para mililitros\n");
+    printf("1. Converter quilogramas para gramas\n");
+    printf("2. Converter gramas para quilogramas\n");
+    printf("3. Converter quilogramas para toneladas\n");
+    printf("4. Converter gramas para toneladas\n");
+    printf("5. Converter toneladas para quilogramas\n");
+    printf("6. Converter toneladas para gramas\n");
     printf("0. Sair\n");
     printf("=================================\n");
 }
